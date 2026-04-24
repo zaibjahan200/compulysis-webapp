489b1ac20459b921d6bd2bcac8766da97f005a61
Jahanzaib Piracha <zaibjahan.200@gmail.com>
2026-04-24 13:57:44 +0500

    added speed insight dependency

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,13 @@
 // frontend/src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
   </React.StrictMode>,
 )
